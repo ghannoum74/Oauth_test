dc5740d49c2298fd33401b784bd1e1a18c291e4b
ghannoum74 <abdelrahmangh74@gmail.com>
2025-06-03 15:39:09 +0300

    refactor structure

---
+++ front-end/.angular/cache/17.3.17/vite/deps/angular-draggable-droppable.js
@@ -0,0 +1,17 @@
+import {
+  DragAndDropModule,
+  DraggableDirective,
+  DraggableScrollContainerDirective,
+  DroppableDirective
+} from "./chunk-KFNMMLNH.js";
+import "./chunk-RW2YPZIO.js";
+import "./chunk-4RCWKJIW.js";
+import "./chunk-Q3XVAEG4.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  DragAndDropModule,
+  DraggableDirective,
+  DraggableScrollContainerDirective,
+  DroppableDirective
+};
+//# sourceMappingURL=angular-draggable-droppable.js.map
---
+++ front-end/.angular/cache/17.3.17/vite/deps/angular-resizable-element.js
@@ -0,0 +1,15 @@
+import {
+  ResizableDirective,
+  ResizableModule,
+  ResizeHandleDirective
+} from "./chunk-3XV7XKAI.js";
+import "./chunk-RW2YPZIO.js";
+import "./chunk-4RCWKJIW.js";
+import "./chunk-Q3XVAEG4.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  ResizableDirective,
+  ResizableModule,
+  ResizeHandleDirective
+};
+//# sourceMappingURL=angular-resizable-element.js.map
